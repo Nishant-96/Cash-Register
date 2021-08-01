0d7ccae08648b3dc2819286a7a521e95ad3216a0
Nishant-96 <nishant11pratihast@gmail.com>
2021-08-02 01:28:03 +0530

    refactor

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven  = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const errorMessage = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven  = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+var errorMessage = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = ["2000","500","100","20","10","5","1"];
+var availableNotes = ["2000","500","100","20","10","5","1"];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
-    errorMessage.style.display = "none";   
+    errorMessage.style.display = "none";    //hides the error the message
     if(billAmount.value > 0){
             if(cashGiven.value >= billAmount.value){
                 var amountToBeReturned = cashGiven.value - billAmount.value;
@@ -22,7 +22,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount(){
            errorMessage.innerText = "The bill amount should be greater than 0."
         }
 });
-
+// proccessing logic
 function calculateChange(amountToBeReturned){
    for(var i=0;i<availableNotes.length;i++){
        var numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
